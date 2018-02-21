061a62fe06078d4fef997862c0e97ffb82d496a5
Ali Zafar <aliszafar@gmail.com>
2018-02-21 22:09:08 +0500

    Added Readme.md

---
+++ android/constants.js
@@ -0,0 +1,6 @@
+export const PLACE = 'PLACE';
+export const MOVE = 'MOVE';
+export const RIGHT = 'RIGHT';
+export const LEFT = 'LEFT';
+export const REPORT = 'REPORT';
+export const INVALID_COMMAND = 'INVALID_COMMAND';
