83b5e6537b783512cf70b524dbc7f427260d8b7a
Ali Zafar <aliszafar@gmail.com>
2018-02-21 19:35:28 +0500

    Implemented unit tests for Actions

---
+++ src/store/robot/__test__/action.spec.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import 'react-native';
+import {
+    robotPlace,
+    robotMove,
+    robotRight,
+    robotLeft,
+    robotReport,
+    invalidCommand
+} from '../action';
+
+import {
+    PLACE,
+    MOVE,
+    RIGHT,
+    LEFT,
+    REPORT,
+    INVALID_COMMAND
+} from '../constants';
+
+describe('Robot Actions', () => {
+    it('robotReport should create PLACE action', () => {
+        expect(robotPlace('PLACE 0,0 EAST')).toEqual({
+            type: PLACE,
+            payload: 'PLACE 0,0 EAST'
+        });
+    });
+
+    it('robotReport should create MOVE action', () => {
+        expect(robotMove()).toEqual({
+            type: MOVE
+        });
+    });
+
+    it('robotReport should create RIGHT action', () => {
+        expect(robotRight()).toEqual({
+            type: RIGHT
+        });
+    });
+
+    it('robotReport should create LEFT action', () => {
+        expect(robotLeft()).toEqual({
+            type: LEFT
+        });
+    });
+
+    it('robotReport should create REPORT action', () => {
+        expect(robotReport()).toEqual({
+            type: REPORT
+        });
+    });
+
+    it('robotReport should create INVALID_COMMAND action', () => {
+        expect(invalidCommand()).toEqual({
+            type: INVALID_COMMAND
+        });
+    });
+});
